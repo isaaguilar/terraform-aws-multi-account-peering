d2cabbd5bd7ff3f32454c6aee54feb44ffe9eded
Isa Aguilar <iaguilar@illumina.com>
2018-02-28 21:36:55 -0800

    examples

--- examples/multi-account-example/main.tf
+++ examples/multi-account-example/main.tf
@@ -1,31 +1,32 @@
-# Terraform AWS multi-account VPC peering module
-
-Set up a peering connection, auto accept, and add to route tables. The request can be same account or between two different accounts.  
-
-
-## Usage 
-
-```
 provider "aws" {
   region = "us-west-2"
-  profile = "account-a"
+  profile = "account-a"       # Profiles assumed to be in ~/.aws/credentials
 }
+
 
 module "multi-account-peering" {
   source  = "isaaguilar/multi-account-peering/aws"
   version = "0.1.0"
+  
   auto_accept_peering = true
   name = "peering-example"
+  
+  
+  # Requester Data
+  
   this_vpc_id = "vpc-dac1daaf"
-  route_table_ids = [ "rtb-05d22c6d", "rtb-17c51a6f" ] 
+  route_table_ids = [ "rtb-05d22c6d", "rtb-17c51a6f" ]  # Set to [] to skip
+  
+  # Accepter Data
+  
   peer_region = "us-east-1"
-  peer_profile = "account-b"
+  peer_profile = "account-b"  # Profiles assumed to be in ~/.aws/credentials
   peer_vpc_id = "vpc-acc6bcff"
   destination_cidr_block = "10.0.1.0/24"
+  
   tags = {
     ManagedWith = "terraform"
     Owner = "Isa Aguilar"
   }
 }
   
-```
