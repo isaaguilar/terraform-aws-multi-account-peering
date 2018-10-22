492800b496f40ac6dac8dead82840f3d0318bd93
Eric Heydrick <eheydrick@gmail.com>
2018-10-22 15:31:01 -0700

    Add descriptions of variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "name" {
-  description = ""
+  description = "Name of the peering connection"
 }
 
 variable "auto_accept_peering" {
-  description = ""
+  description = "Whether to auto-accept the peering request"
   default = true
 }
 
 variable "tags" {
   type = "map"
-  description = ""
+  description = "Tags to apply"
 }
 
 ####################
@@ -17,15 +17,15 @@ variable "tags" {
 ####################
 
 variable "this_vpc_id" {
-  description = ""
+  description = "VPC ID of the requestor"
 }
 
 variable "this_cidr_block" {
-  description = ""
+  description = "CIDR block of the requestor"
 }
 
 variable "this_route_table_ids" {
-  description = ""
+  description = "Route table IDs of the requestor"
   type = "list"
   default = []
 }
@@ -35,23 +35,23 @@ variable "this_route_table_ids" {
 ###################
 
 variable "peer_vpc_id" {
-  description = ""
+  description = "VPC ID of the peer"
 }
 
 variable "peer_region" {
-  description = ""
+  description = "Region of the peer"
 }
 
 variable "peer_profile" {
-  description = ""
+  description = "Profile of the peer"
 }
 
 variable "peer_cidr_block" {
-  description = ""
+  description = "CIDR block of the peer"
 }
 
 variable "peer_route_table_ids" {
-  description = ""
+  description = "Route table IDs of the peer"
   type = "list"
   default = []
 }
